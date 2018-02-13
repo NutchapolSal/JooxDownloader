fe414e396a10dd251a9969626dfb24117c0e9ca5
Nutchapol Salawej <nutchapol.sal@gmail.com>
2018-02-13 18:20:37 +0700

    Fixed songinfo.sh

--- data/songinfo.sh
+++ data/songinfo.sh
@@ -1,1 +1,1 @@
-< input.txt sed '/#/d' | sed 's/ //g' | sed '/^ *$/d' | tr \& '\n' | sed '/ksongid/d' | grep 'songid' | sed 's/songid=//' | sed 's/^/http:\/\/api.joox.com\/web-fcgi-bin\/web_get_songinfo?songid=/' | xargs wget -P ./temp
+< input.txt sed '/#/d' | sed 's/ //g' | sed '/^ *$/d' | sed 's/.*\&songid=//' | sed 's/\==*.*./==/' | sed 's/.*song\///' | sed 's/\/lyr*....//' | sed 's/^/http:\/\/api.joox.com\/web-fcgi-bin\/web_get_songinfo?songid=/' | xargs wget -P ./temp
